905edb0cf915260d13166c793c98ee3be460c95d
ramazanova_guzalliia <ramazanova_guzalliia@mail.ru>
2026-09-16 13:32:37 +0500

    feat: tasks 1, 2

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -19,8 +19,14 @@
  *   getStringLength(null) => 0
  *   getStringLength(undefined) => 0
  */
-function getStringLength(/* value */) {
-  throw new Error('Not implemented');
+function getStringLength(value) {
+  let number = 0;
+
+  if (typeof value === 'string') {
+    number = value.length;
+  }
+
+  return number;
 }
 
 /**
@@ -37,8 +43,12 @@ function getStringLength(/* value */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  if (typeof value === 'string' || value instanceof String) {
+    return true;
+  }
+
+  return false;
 }
 
 /**
